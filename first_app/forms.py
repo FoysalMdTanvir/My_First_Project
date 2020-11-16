64a8746747a438a13767838cc12646f7df201c52
Md. Tanvir Foysal <tanvirfaisal54@gmail.com>
2020-11-16 17:18:12 +0600

    Class based View

--- first_app/forms.py
+++ first_app/forms.py
@@ -1,14 +1,17 @@
 from django import forms
 from first_app import models
 
+
 class MusicianForm(forms.ModelForm):
     class Meta:
         model = models.Musician
         fields = "__all__"
 
+
 class AlbumForm(forms.ModelForm):
-    release_date = forms.DateField(widget=forms.TextInput(attrs={'type':'date'}))
+    release_date = forms.DateField(widget=forms.TextInput(attrs={'type': 'date'}))
     name = forms.CharField(label='Name Of Album')
+
     class Meta:
         model = models.Album
         fields = "__all__"
