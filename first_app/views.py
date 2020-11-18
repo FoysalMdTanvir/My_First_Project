3af74e482a0b06916426a85c64bfdd59b4b7f820
Md. Tanvir Foysal <tanvirfaisal54@gmail.com>
2020-11-18 23:05:06 +0600

    CBV-4

--- first_app/views.py
+++ first_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import View, TemplateView, ListView, DetailView, CreateView
+from django.views.generic import View, TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
 from first_app import models
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -22,3 +23,16 @@ class AddMusician(CreateView):
     fields = ('first_name', 'last_name', 'instrument')
     model = models.Musician
     template_name = 'first_app/musician_form.html'
+
+
+class UpdateMusician(UpdateView):
+    fields = ('first_name', 'last_name', 'instrument')
+    model = models.Musician
+    template_name = 'first_app/musician_form.html'
+
+
+class DeletMusician(DeleteView):
+    context_object_name = 'musician'
+    model = models.Musician
+    success_url = reverse_lazy("first_app:index")
+    template_name = 'first_app/delete_musician.html'
